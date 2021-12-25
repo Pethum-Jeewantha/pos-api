71aa9f04f66b8da2d94531cddfce9e0f7486680f
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-12-25 11:21:44 +0530

    add CustomerController & ItemController

---
+++ src/main/java/com/pethumjeewanha/pos/api/CustomerController.java
@@ -0,0 +1,81 @@
+package com.pethumjeewanha.pos.api;
+
+import com.pethumjeewanha.pos.dto.CustomerDTO;
+import com.pethumjeewanha.pos.service.custom.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@CrossOrigin
+@RequestMapping("/api/v1/customers")
+@RestController
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<CustomerDTO> getAllCustomers() {
+        return customerService.findAllCustomers();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, params = {"page", "size"})
+    public ResponseEntity<List<CustomerDTO>> getAllCustomersByPageAndSize(@RequestParam int page,
+                                                                          @RequestParam int size) {
+        if (page <= 0 || size <= 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid page or size");
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Count", customerService.getCustomersCount() + "");
+        return new ResponseEntity<List<CustomerDTO>>(customerService.findAllCustomers(page, size), headers, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id:C\\d{3}}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CustomerDTO getCustomer(@PathVariable String id) {
+        return customerService.findCustomer(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String saveCustomer(@RequestBody CustomerDTO customer) {
+
+        if (customer.getId() == null || !customer.getId().matches("C\\d{3}")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid customer id");
+        } else if (customer.getName() == null || customer.getName().trim().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid customer name");
+        } else if (customer.getAddress() == null || customer.getAddress().trim().length() < 3) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid customer address");
+        }
+
+        customerService.saveCustomer(customer);
+        return customer.getId();
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping(value = "/{id:C\\d{3}}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateCustomer(@RequestBody CustomerDTO customer, @PathVariable String id) {
+        if (customer.getId() != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid request pay load");
+        } else if (customer.getName() == null || customer.getName().trim().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid customer name");
+        } else if (customer.getAddress() == null || customer.getAddress().trim().length() < 3) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid customer address");
+        }
+
+        customer.setId(id);
+        customerService.updateCustomer(customer);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "/{id:C\\d{3}}")
+    public void deleteCustomer(@PathVariable String id) {
+        customerService.deleteCustomer(id);
+    }
+}
---
+++ src/main/java/com/pethumjeewanha/pos/api/ItemController.java
@@ -0,0 +1,84 @@
+package com.pethumjeewanha.pos.api;
+
+import com.pethumjeewanha.pos.dto.ItemDTO;
+import com.pethumjeewanha.pos.service.custom.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@CrossOrigin
+@RequestMapping("/api/v1/items")
+@RestController
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<ItemDTO> getAllItems() {
+        return itemService.findAllItems();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, params = {"page", "size"})
+    public ResponseEntity<List<ItemDTO>> getAllItemsByPageAndSize(@RequestParam int page,
+                                                                  @RequestParam int size) {
+        if (page <= 0 || size <= 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid page or size");
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Count", itemService.getItemsCount() + "");
+        return new ResponseEntity<>(itemService.findAllItems(page, size), headers, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{code:I\\d{3}}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ItemDTO getItem(@PathVariable String code) {
+        return itemService.findItem(code);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String saveItem(@RequestBody ItemDTO item) {
+
+        if (item.getCode() == null || !item.getCode().matches("I\\d{3}")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid item code");
+        } else if (item.getDescription() == null || item.getDescription().trim().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid description");
+        } else if (item.getQtyOnHand() <= 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid qty");
+        } else if (item.getUnitPrice() == null || item.getUnitPrice().compareTo(new BigDecimal("0")) <= 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid unit price");
+        }
+
+        itemService.saveItem(item);
+        return item.getCode();
+    }
+
+    @PutMapping(value = "/{code:I\\d{3}}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateItem(@RequestBody ItemDTO item, @PathVariable String code) {
+
+        if (item.getCode() != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid request payload");
+        } else if (item.getDescription() == null || item.getDescription().trim().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid description");
+        } else if (item.getQtyOnHand() <= 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid qty");
+        } else if (item.getUnitPrice() == null || item.getUnitPrice().compareTo(new BigDecimal("0")) <= 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid unit price");
+        }
+
+        item.setCode(code);
+        itemService.updateItem(item);
+    }
+
+    @DeleteMapping(value = "/{code:I\\d{3}}")
+    public void deleteItem(@PathVariable String code) {
+        itemService.deleteItem(code);
+    }
+}
