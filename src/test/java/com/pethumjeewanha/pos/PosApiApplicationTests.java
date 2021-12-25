916c1461475358c71f7106a4a79fdcb44e7ec1fc
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-12-25 09:37:34 +0530

    initial commit

---
+++ src/test/java/com/pethumjeewanha/pos/PosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pethumjeewanha.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
