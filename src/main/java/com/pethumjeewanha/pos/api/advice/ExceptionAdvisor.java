7938d016049a0ec0717f959f1a44e10f635c0039
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-12-25 23:08:42 +0530

    add ExceptionAdvisor

---
+++ src/main/java/com/pethumjeewanha/pos/api/advice/ExceptionAdvisor.java
@@ -0,0 +1,22 @@
+package com.pethumjeewanha.pos.api.advice;
+
+import com.pethumjeewanha.pos.exception.DuplicateEntityException;
+import com.pethumjeewanha.pos.exception.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class ExceptionAdvisor {
+
+    @ExceptionHandler(RuntimeException.class)
+    public void handleExceptions(RuntimeException ex) {
+        if (ex instanceof EntityNotFoundException) {
+            ex.initCause(new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage()));
+        } else if (ex instanceof DuplicateEntityException) {
+            ex.initCause(new ResponseStatusException(HttpStatus.CONFLICT, ex.getMessage()));
+        }
+        throw ex;
+    }
+}
